7be68f81378b0163ec2aaf3ea88f273675a5a952
Gonzalo de Dios <ghdedios@hotmail.com>
2018-12-06 09:34:38 -0300

    Main Activity preliminar Layout

--- app/src/main/java/dadm/frba/utn/edu/ar/ufceventfinder/MainActivity.java
+++ app/src/main/java/dadm/frba/utn/edu/ar/ufceventfinder/MainActivity.java
@@ -2,12 +2,36 @@ package dadm.frba.utn.edu.ar.ufceventfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mUFCEvent;
+    private TextView mErrorMessageDisplay;
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUFCEvent = (TextView) findViewById(R.id.tv_tittle);
+        mUFCEvent.setText("UFC Event Dummy");
+
+        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+
+    }
+
+    private void showErrorMessage(){
+        mUFCEvent.setVisibility(View.INVISIBLE);
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+    private void showUFCEvents(){
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        mUFCEvent.setVisibility(View.VISIBLE);
     }
 }

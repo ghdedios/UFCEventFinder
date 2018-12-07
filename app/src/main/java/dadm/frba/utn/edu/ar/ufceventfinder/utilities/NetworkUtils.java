f2792034ff3481e7f7ee59b519789a9087b0dddf
Gonzalo de Dios <ghdedios@hotmail.com>
2018-12-07 09:21:02 -0300

    getResponseFromURL method in network utils

--- app/src/main/java/dadm/frba/utn/edu/ar/ufceventfinder/utilities/NetworkUtils.java
+++ app/src/main/java/dadm/frba/utn/edu/ar/ufceventfinder/utilities/NetworkUtils.java
@@ -2,8 +2,12 @@ package dadm.frba.utn.edu.ar.ufceventfinder.utilities;
 
 import android.net.Uri;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 public class NetworkUtils {
     final static String UFC_EVENT_URL = "http://ufc-data-api.ufc.com/api/v3/iphone/events";
@@ -11,6 +15,7 @@ public class NetworkUtils {
     /**
      * Builds URL from static route defined in UFC_EVENT_URL. If a search query is needed,
      * you only have to add the appendQueryParameter() between buildUpon() and build()
+     * @return the UFC event finder URL
      */
 
     public static URL buildUrl() {
@@ -24,5 +29,29 @@ public class NetworkUtils {
         return url;
     }
 
-    
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 }

76c800760e469ae7b721799c317f073488da05c3
Gonzalo de Dios <ghdedios@hotmail.com>
2018-12-07 09:29:21 -0300

    Menu refresh option

--- app/src/main/java/dadm/frba/utn/edu/ar/ufceventfinder/MainActivity.java
+++ app/src/main/java/dadm/frba/utn/edu/ar/ufceventfinder/MainActivity.java
@@ -2,6 +2,8 @@ package dadm.frba.utn.edu.ar.ufceventfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -33,5 +35,21 @@ public class MainActivity extends AppCompatActivity {
     private void showUFCEvents(){
         mErrorMessageDisplay.setVisibility(View.INVISIBLE);
         mUFCEvent.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClicked = item.getItemId();
+        if(itemThatWasClicked == R.id.action_refresh){
+            //TODO: replace with making the search again
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
